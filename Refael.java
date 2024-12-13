7e86eaa7b421f90a3151204fcf90a4f6ba564aea
refael-1324 <refael1324@gmail.com>
2024-12-13 13:04:09 +0200

    modified Refael.java

--- Refael.java
+++ Refael.java
@@ -1,7 +1,7 @@
 public class Refael{
 	public static void main(String[]args){
 			for(int i=1; i<=10; i++){
-					System.out.println("Refael"+i);
+					System.out.println("Hello harry potter my name is Tom Riddle"+i);
 			}
 	}
 }	
